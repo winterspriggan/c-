0239ff8524bcdd2de304304b1b5bb15bfc4db125
winterspriggan <jin-_-x@naver.com>
2023-12-21 18:47:35 +0900

    20231221

---
+++ 231221/main.cpp
@@ -0,0 +1,13 @@
+// #include : 헤더파일을 여기에 포함시키는 기능이다.
+#include <iostream>
+
+// 실행 과정 : 컴파일 -> 빌드 -> 실행
+//컴파일 : 번역 작업이다.
+
+//C++ 의 시작점은 main 함수이다. main 함수는 반드시 있어야 한다.
+int main()
+{
+    std::cout << "Test Output";
+
+    return 0;
+}
